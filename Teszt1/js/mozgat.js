7fe940052e0ddd0a1a1c25052ba8f286a866cd4c
Nyolczas <nyolczas@gmail.com>
2019-03-29 21:45:36 +0100

    kész

--- Teszt1/js/mozgat.js
+++ Teszt1/js/mozgat.js
@@ -1,11 +1,43 @@
-anime({
-    targets: '.polymorph',
-    points: [
-      { value: '838.5,0.5 1263,531 839,1080.5 1164,1080.5 1587,524 1163.5,0.5'},
-      { value: '838.5,0.5 600.2,167.2 839,1080.5 1164,1080.5 917.8,231.8 1163.5,0.5'},
-      { value: '838.5,0.5 838,531 839,1080.5 1164,1080.5 1162,524 1163.5,0.5'},
-    ],
-    easing: 'linear',
-    duration: 2000,
-    loop: true
-  });
+var btnStart = document.getElementById('btn-start');
+var btnBack = document.getElementById('btn-back');
+var animDuration = 500;
+
+btnStart.onclick = function() {
+    btnStart.style.display='none';
+    
+    anime({
+        targets: '.polymorph',
+        points: [
+            {
+                value: '838.5,0.5 1263,531 839,1080.5 1164,1080.5 1587,524 1163.5,0.5'
+            },
+        ],
+        delay: anime.stagger(10, {easing: 'easeOutQuad'}),
+        // duration: animDuration,
+        loop: false
+    });
+
+    setTimeout(function() {
+        btnBack.style.display = 'inline-block';
+    }, animDuration);
+}
+
+btnBack.onclick = function() {
+    btnBack.style.display='none';
+    
+    anime({
+        targets: '.polymorph',
+        points: [
+            {
+                value: '838.5,0.5 838,531 839,1080.5 1164,1080.5 1162,524 1163.5,0.5'
+            },
+        ],
+        delay: anime.stagger(10, {easing: 'easeOutQuad'}),
+        // duration: animDuration,
+        loop: false
+    });
+
+    setTimeout(function() {
+        btnStart.style.display = 'inline-block';
+    }, animDuration);
+}
